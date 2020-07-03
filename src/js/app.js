7b32e9a50ad8c67c80f9d622ff8da24a25abe367
Gale King <grexkingiv@gmail.com>
2020-07-03 17:17:42 -0400

    Initial commit without styling

---
+++ src/js/app.js
@@ -0,0 +1,103 @@
+
+
+let donutCount = 0;
+let donutClickButton = document.querySelector("#donut-click-button");
+let numberOfDonuts = document.querySelector(".number-of-donuts");
+let buyAutoClickButton = document.querySelector("#auto-clicker-buy-button");
+
+let buyDonutMultiplierButton = document.querySelector("#multiplier-buy-button")
+let autoClickerCount = 0;
+let autoClickerPrice = 100;
+let multiplierCount = 0;
+let multiplierPrice = 10;
+let numberOfAutoClickers = document.querySelector(".number-of-autoclickers");
+let numberOfMultipliersTag = document.querySelector(".number-of-multipliers");
+let multiplierPriceTag = document.querySelector(".multiplier-price-tag");
+let autoClickerPriceTag = document.querySelector(".autoclicker-price-tag");
+let resetButton = document.querySelector("#reset-button");
+
+document.getElementById("auto-clicker-buy-button").disabled = true;
+document.getElementById("multiplier-buy-button").disabled = true;
+
+
+donutClickButton.addEventListener("click", addDonut);
+buyAutoClickButton.addEventListener("click", buyAutoClicker);
+buyDonutMultiplierButton.addEventListener("click", buyDonutMultiplier);
+resetButton.addEventListener("click", resetButtonClick);
+
+
+function addDonut() {
+  //  disableButtons();
+  donutCount += Math.round(Math.pow(1.2, multiplierCount));
+  numberOfDonuts.innerText = Math.round(donutCount);
+  disableButtons();
+  
+}
+
+
+
+function fireAutoClicker(){
+setInterval(function(){
+  disableButtons();
+  donutCount += Math.round(autoClickerCount * Math.pow(1.2,multiplierCount)); 
+  numberOfDonuts.innerText = Math.round(donutCount)
+}, 1000); }
+
+function buyAutoClicker(){
+  // disableButtons();
+  if (donutCount >= autoClickerPrice){
+    autoClickerCount +=1;
+    donutCount -= autoClickerPrice;
+    autoClickerPrice = Math.round(autoClickerPrice * 1.1);
+    numberOfDonuts.innerText = Math.round(donutCount);
+    autoClickerPriceTag.innerText = autoClickerPrice;
+    numberOfAutoClickers.innerText = autoClickerCount;
+    if (autoClickerCount <= 1){
+      fireAutoClicker ();
+
+    }
+    disableButtons();
+  }
+}
+function buyDonutMultiplier(){
+  // disableButtons();
+  if(donutCount>= multiplierPrice){
+    multiplierCount +=1;
+    donutCount -= multiplierPrice;
+    multiplierPrice = Math.round(multiplierPrice *1.1);
+    numberOfDonuts.innerText = Math.round(donutCount);
+    multiplierPriceTag.innerText = multiplierPrice;
+    numberOfMultipliersTag.innerText = multiplierCount;
+    disableButtons();
+    
+  }
+}
+function disableButtons(){
+  if(donutCount < multiplierPrice){
+    document.getElementById("multiplier-buy-button").disabled = true;
+  } else {
+    document.getElementById("multiplier-buy-button").disabled = false;
+
+  }
+  if(donutCount < autoClickerPrice){
+    document.getElementById("auto-clicker-buy-button").disabled = true;
+  } else{
+    document.getElementById("auto-clicker-buy-button").disabled = false;
+  }
+}
+function resetButtonClick(){
+  donutCount = 0;
+  numberOfDonuts.innerText = Math.round(donutCount);
+ autoClickerCount = 0;
+ numberOfAutoClickers.innerText = autoClickerCount;
+ autoClickerPrice = 100;
+ autoClickerPriceTag.innerText = autoClickerPrice;
+ multiplierCount = 0;
+ numberOfMultipliersTag.innerText = multiplierCount;
+ multiplierPrice = 10;
+ multiplierPriceTag.innerText = multiplierPrice;
+ disableButtons();
+//  clearInterval();
+//  fireAutoClicker.disabled;
+
+}
