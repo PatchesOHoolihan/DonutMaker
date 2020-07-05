e83835bc981d604c482280edc712c455f53eeb3d
Gale King <grexkingiv@gmail.com>
2020-07-05 18:01:20 -0400

    Sunday 6PM push

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,8 @@
 
-
 let donutCount = 0;
 let donutClickButton = document.querySelector("#donut-click-button");
 let numberOfDonuts = document.querySelector(".number-of-donuts");
 let buyAutoClickButton = document.querySelector("#auto-clicker-buy-button");
-
 let buyDonutMultiplierButton = document.querySelector("#multiplier-buy-button")
 let autoClickerCount = 0;
 let autoClickerPrice = 100;
@@ -15,26 +13,35 @@ let numberOfMultipliersTag = document.querySelector(".number-of-multipliers");
 let multiplierPriceTag = document.querySelector(".multiplier-price-tag");
 let autoClickerPriceTag = document.querySelector(".autoclicker-price-tag");
 let resetButton = document.querySelector("#reset-button");
+const modal = document.getElementById("modal-id");
+const span = document.getElementsByClassName("close")[0];
+const modalClick = document.getElementById("developer-information");
+modalClick.onclick = function(){
+    modal.style.display = "block";
+  }
+  span.onclick = function(){
+    modal.style.display = "none";
+  }
+  window.onclick = function(event){
+    if(event.target == modal){
+      modal.style.display = "none";
+    }
+}
 
 document.getElementById("auto-clicker-buy-button").disabled = true;
 document.getElementById("multiplier-buy-button").disabled = true;
-
 
 donutClickButton.addEventListener("click", addDonut);
 buyAutoClickButton.addEventListener("click", buyAutoClicker);
 buyDonutMultiplierButton.addEventListener("click", buyDonutMultiplier);
 resetButton.addEventListener("click", resetButtonClick);
 
-
 function addDonut() {
-  //  disableButtons();
+  
   donutCount += Math.round(Math.pow(1.2, multiplierCount));
   numberOfDonuts.innerText = Math.round(donutCount);
   disableButtons();
-  
 }
-
-
 
 function fireAutoClicker(){
 setInterval(function(){
@@ -44,7 +51,6 @@ setInterval(function(){
 }, 1000); }
 
 function buyAutoClicker(){
-  // disableButtons();
   if (donutCount >= autoClickerPrice){
     autoClickerCount +=1;
     donutCount -= autoClickerPrice;
@@ -60,7 +66,7 @@ function buyAutoClicker(){
   }
 }
 function buyDonutMultiplier(){
-  // disableButtons();
+  
   if(donutCount>= multiplierPrice){
     multiplierCount +=1;
     donutCount -= multiplierPrice;
@@ -69,7 +75,6 @@ function buyDonutMultiplier(){
     multiplierPriceTag.innerText = multiplierPrice;
     numberOfMultipliersTag.innerText = multiplierCount;
     disableButtons();
-    
   }
 }
 function disableButtons(){
@@ -77,7 +82,6 @@ function disableButtons(){
     document.getElementById("multiplier-buy-button").disabled = true;
   } else {
     document.getElementById("multiplier-buy-button").disabled = false;
-
   }
   if(donutCount < autoClickerPrice){
     document.getElementById("auto-clicker-buy-button").disabled = true;
@@ -86,18 +90,5 @@ function disableButtons(){
   }
 }
 function resetButtonClick(){
-  donutCount = 0;
-  numberOfDonuts.innerText = Math.round(donutCount);
- autoClickerCount = 0;
- numberOfAutoClickers.innerText = autoClickerCount;
- autoClickerPrice = 100;
- autoClickerPriceTag.innerText = autoClickerPrice;
- multiplierCount = 0;
- numberOfMultipliersTag.innerText = multiplierCount;
- multiplierPrice = 10;
- multiplierPriceTag.innerText = multiplierPrice;
- disableButtons();
-//  clearInterval();
-//  fireAutoClicker.disabled;
-
+  location.reload();
 }
